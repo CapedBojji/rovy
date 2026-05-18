8ac83418b4808ad2069f46fcdeb9f3eaa86f4878
Reikan404 <Reikan404@gmail.com>
2026-05-18 11:33:54 -0400

    chore(examples): fix indentation in destroy-model monitor; add sourcemap

--- examples/zombie-game/src/client/monitors/destroy-model.ts
+++ examples/zombie-game/src/client/monitors/destroy-model.ts
@@ -10,15 +10,17 @@ import { Model } from "../components";
  */
 @monitor({ match: query<[Model]>() })
 export class DestroyModelOnExit {
-	private readonly parts = new Map<Entity, Part>();
+  private readonly parts = new Map<Entity, Part>();
 
-	onEnter(entity: Entity, model: Model): void {
-		this.parts.set(entity, model.part);
-	}
+  onEnter(entity: Entity, model: Model): void {
+    this.parts.set(entity, model.part);
+  }
 
-	onExit(entity: Entity, _model: Model): void {
-		const part = this.parts.get(entity);
-		if (part !== undefined) part.Destroy();
-		this.parts.delete(entity);
-	}
+  onExit(entity: Entity, _model: Model): void {
+    const part = this.parts.get(entity);
+    if (part !== undefined) part.Destroy();
+    this.parts.delete(entity);
+  }
 }
+
+
